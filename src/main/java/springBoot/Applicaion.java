36f0100e9a3107edda3eee6e5f4d3b2e9867f97b
ChiaFu Kuo <fatfatfu@gmail.com>
2018-02-07 22:45:00 +0800

    lst commit

---
+++ src/main/java/springBoot/Applicaion.java
@@ -0,0 +1,14 @@
+package springBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Applicaion {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Applicaion.class,args);
+
+	}
+
+}
